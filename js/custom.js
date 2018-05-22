ab6f463fd29d88cad269b440a84c660f1259be02
Nikita <nikitainduleek@hotmail.com>
2018-05-22 22:20:32 +0100

    jquery added to index

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,4 @@
 
-
 // slideshow: w3 schools
 var myIndex = 0;
 carousel();
@@ -15,3 +14,39 @@ function carousel() {
     x[myIndex-1].style.display = "block";
     setTimeout(carousel, 4000);
 }
+
+// sign up: my own jquery
+
+  $(document).ready(function(){
+
+    $("#newslettersignup").click(function(){
+        $("#signup").show();
+    });
+
+    $("button").click(function(){
+        $("#signup").hide();
+    });
+
+  });
+
+  // events bar my own jquery
+
+  $(document).ready(function(){
+
+    $("#eventbar1 img").click(function(){
+        $("#eventbar1 > p").toggle();
+    });
+
+    $("#eventbar2 img").click(function(){
+        $("#eventbar2 > p").toggle();
+    });
+
+    $("#eventbar3 img").click(function(){
+        $("#eventbar3 > p").toggle();
+    });
+
+    $("#eventbar4 img").click(function(){
+        $("#eventbar4 > p").toggle();
+    });
+
+  });
